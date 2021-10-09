07257c21c38c68b8706da19edde07c5c94f62269
ironyse <luk3man.lh@gmail.com>
2021-10-09 15:56:49 +0700

    improving enemy AI

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -7,8 +7,15 @@ public class EnemyAI : MonoBehaviour
     public float speed = 5f;
     public float shootDistance;
     public float retreatDistance;
+    public float offsetDistance;
 
     [SerializeField] private Transform player;
+    [SerializeField] private Transform firepoint;
+    [SerializeField] private GameObject laserPrefab;
+
+    public float laserForce = 0.5f;
+    public float shootDelay;
+    private float _shootTimer;
 
     Vector2 targetRandomPosition;
 
@@ -21,22 +28,16 @@ public class EnemyAI : MonoBehaviour
     private void Update()
     {
         float distance = Vector2.Distance(transform.position, player.position);
+        _shootTimer -= Time.deltaTime;
 
         if (distance> shootDistance) {
-            Chase();
-            targetRandomPosition = GetRandomPosition();
+            Chase();            
 
-        }  else if (distance > retreatDistance && distance < shootDistance) {            
-            RandomMove();
-            if (Vector2.Distance(transform.position, targetRandomPosition) < 0.1f)
-            {
-                targetRandomPosition = GetRandomPosition();
-            }
+        }  else if (distance > retreatDistance  && distance < shootDistance) {            
+            RandomMove();            
 
         } else if (distance < retreatDistance) {            
             Retreat();
-            targetRandomPosition = GetRandomPosition();
-
         }
 
 
@@ -54,17 +55,40 @@ public class EnemyAI : MonoBehaviour
     private void Chase()
     {
         transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        targetRandomPosition = GetRandomPosition();
     }
 
     private void RandomMove()
-    {        
+    {
+        Shoot();
         transform.position = Vector2.MoveTowards(transform.position, targetRandomPosition, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, targetRandomPosition) < 0.1f)
+        {
+            targetRandomPosition = GetRandomPosition();
+        }
     }
 
     private void Retreat()
     {
+        Shoot();
         transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        targetRandomPosition = GetRandomPosition();
     }
+
+    private void Shoot()
+    {
+        if (_shootTimer <= 0f)
+        {
+            GameObject laser = Instantiate(laserPrefab, firepoint.position, firepoint.rotation);
+            Rigidbody2D rbLaser = laser.GetComponent<Rigidbody2D>();
+            rbLaser.AddForce(firepoint.up * laserForce, ForceMode2D.Impulse);
+
+            _shootTimer = shootDelay;
+        }
+        
+    }
+
 
     private Vector3 GetRandomPosition()
     {
